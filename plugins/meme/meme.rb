17275170dcf97dc47fb34fb21c5c65a503c34c36
Chris Odlaug <codlaug@wetpaint-inc.com>
2013-10-17 09:16:49 -0700

    Adding dave silverman rageface

--- plugins/meme/meme.rb
+++ plugins/meme/meme.rb
@@ -14,7 +14,8 @@ class Meme < Isis::Plugin::Base
     '!massimo' => 'http://i0.kym-cdn.com/entries/icons/original/000/000/567/Massimo.jpg',
     '!clap' => ['http://i9.photobucket.com/albums/a81/Gn4rk1ll/citizen-kane-clapping.gif', 
                 'http://myscriptx.com/dl/94/seinfeld-clap.gif'],
-    '!disaster' => 'http://i3.kym-cdn.com/photos/images/original/000/000/130/disaster-girl.jpg'
+    '!disaster' => 'http://i3.kym-cdn.com/photos/images/original/000/000/130/disaster-girl.jpg',
+    '!silverman' => 'http://i0.kym-cdn.com/entries/icons/original/000/005/545/OpoQQ.jpg'
   }
 
   TRIGGERS = TRIGGER_TO_IMAGE_URL.keys
