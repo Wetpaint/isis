5b918600002ba2ad70fdbb180a6a7eb5871f06bf
Chris Odlaug <codlaug@wetpaint-inc.com>
2013-11-14 15:48:49 -0800

    Tumblr plugin

---
+++ plugins/tumblr/tumblr.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+
+class DevopsReactions < Isis::Plugin::Base
+
+  TRIGGERS = %w(!tumblr)
+
+  def respond_to_msg?(msg, speaker)
+    TRIGGERS.include? msg.split(' ')[0].downcase
+    @domain = msg.split(' ')[1]
+  end
+
+  private
+
+  def response_text
+    page = Nokogiri.HTML(open("http://#{@domain}.tumblr.com/random"))
+    title = page.title
+    img = page.css('#content img')
+    img = img[0] if img.is_a? Array
+    img = img.attr('src').value
+    "#{title} #{img}"
+  end
+end
